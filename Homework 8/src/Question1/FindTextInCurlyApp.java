343edd2622f72d0e81f81a689b233872103b6374
Eylul Rojin <eylulrojingunduz@gmail.com>
2023-10-24 16:15:01 +0300

    Homework 8 question 1 was added.

---
+++ Homework 8/src/Question1/FindTextInCurlyApp.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	Klavyeden bir yazı giriniz. Yazı içerisinde iç içe küme parantezleri bulunuyor olsun. En içteki parantezinin
+	içerisindeki yazıyı yazdırınız.
+	Aşağıdaki örnek yazıları kopyalayıp yapıştırabilirsiniz:
+	- Bu örnek {aslında {hiç zor} değil}
+	- Bu örnekten {daha {zor {örnekler de yapacağız} değil}} mi?
+	- Yanlış bir {giriş için hata mesajı yazdırılacak
+	- Bu örnekten {daha
+	- Bu örnekten daha zor }örnekler
+	- {test}
+	- {{test}}
+	- {}
+	- Bu örnekten }daha zor {örnekler
+-----------------------------------------------------------------------------------------------------------------------*/
+package Question1;
+
+import java.util.Scanner;
+
+public class FindTextInCurlyApp {
+    public static void run()
+    {
+        Scanner kb = new Scanner(System.in);
+
+        for (;;) {
+            System.out.print("Bir yazı giriniz:");
+            String s = kb.nextLine();
+
+            if ("elma".equals(s))
+                break;
+
+            FindTextInCurly findTextInCurly = new FindTextInCurly(s);
+
+            findTextInCurly.parse();
+
+            System.out.println(findTextInCurly.isValid ? findTextInCurly.result : "yanlış bir giriş");
+
+        }
+
+        System.out.println("Tekrar yapıyor musunuz?");
+    }
+}
