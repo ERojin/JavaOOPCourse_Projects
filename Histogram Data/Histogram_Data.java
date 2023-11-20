c246e2b76db632515f326d2fa286d512f44c9f6f
Eylul Rojin Yılmaz <97739030+ERojin@users.noreply.github.com>
2023-11-20 11:30:49 +0300

    Histogram Data was updated.

---
+++ Histogram Data/Histogram_Data.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------------------------------------------------
+Aşağıdaki açıklamalara göre getHistogramData isimli metodu ArrayUtil sınıfı içerisinde yazınız
+ve aşağıdaki kod ile test ediniz:
+Açıklamalar:
+	- Metot int türden bir dizi ve int türden bir n sayısı alacaktır:
+	int [] getHistogramData(int [] a, int n)
+
+	- Metot a dizisi içerisinde [0, n] aralığındaki sayılardan jhangisinin kaç tane olduğunu içeren bir sayaç dizisine
+	geri dönecektir
+
+	- Sayaç dizisinin her bir indeks numarası [0, n] aralığındaki sayıyı temsil edecektir. Yani örneğinb sıfır numaralı
+	indeksteki değer sıfır sdayısının kaç tane olduğu bilgisini içerecektir
+-----------------------------------------------------------------------------------------------------------------------*/
+package histogramdata;
+
+import static java.lang.Math.floor;
+import static org.csystem.util.array.ArrayUtil.max;
+
+public class Histogram_Data {
+    public static int [] getHistogramData(int [] a, int n)
+{
+    int [] counts = new int[n+1];
+
+        for (int val : a)
+            ++counts[val];
+
+        return counts;
+    }
+
+    public static void drawHistogram(int [] data, int count, char ch)
+    {
+        int maxVal = max(data);
+
+        for (int val : data) {
+            int charCount = (int)floor(val * count / (double) maxVal);
+
+            while (charCount -- > 0)
+                System.out.print(ch);
+
+            System.out.println();
+        }
+
+    }
+
+}
