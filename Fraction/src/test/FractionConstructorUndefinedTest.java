f59623c5b870038298c02120c0a1c4e3db1c2f39
Eylul Rojin Yılmaz <97739030+ERojin@users.noreply.github.com>
2024-03-11 16:54:55 +0300

    Update FractionConstructorUndefinedTest.java

--- Fraction/src/test/FractionConstructorUndefinedTest.java
+++ Fraction/src/test/FractionConstructorUndefinedTest.java
@@ -5,8 +5,14 @@ import math.Fraction;
 public class FractionConstructorUndefinedTest {
     public static void run()
     {
-       Fraction f = new Fraction(10,0);
+        try {
+            Fraction fraction = new Fraction(10, 0);
+        }
+        catch (NumberFormatException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
+
     public static void main(String[] args)
     {
         run();
